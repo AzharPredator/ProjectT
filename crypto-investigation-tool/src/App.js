12e9fe332b2f8d5518df1dbaa530f7bd54e6ae1a
AzharPredator <thiruhari2323@gmail.com>
2023-09-11 23:17:06 +0530

    11-09-2023

---
+++ crypto-investigation-tool/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import InvestigationTool from './components/InvestigationTool';
+
+function App() {
+  return (
+    <div className="App">
+      <InvestigationTool />
+    </div>
+  );
+}
+
+export default App;
